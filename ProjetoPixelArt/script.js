076df2c14bd44b76ab1fa61f79086a4a7acbfd82
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-09-14 23:15:02 -0300

    Atualiza a página do projeto PixelArt

--- ProjetoPixelArt/script.js
+++ ProjetoPixelArt/script.js
@@ -25,11 +25,58 @@ function limpaQuadro() {
   }
 }
 
+function geraQuadro(tamanho) {
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.style.gridTemplateColumns = 'repeat(' + tamanho + ', 40px)';
+  pixelBoard.style.gridTemplateRows = 'repeat(' + tamanho + ', 40px)';
+  let numeroDePixels = parseInt(tamanho);
+  numeroDePixels *= numeroDePixels;
+  let ultimoPixel = pixelBoard.lastElementChild;
+  while (ultimoPixel) {
+    pixelBoard.removeChild(ultimoPixel);
+    ultimoPixel = pixelBoard.lastElementChild;
+  }
+  for (let index = 0; index < numeroDePixels; index += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
+}
+
+function checaValorTamanho() {
+  let valorInput = document.querySelector('#board-size').value;
+  const tamanho = parseInt(valorInput);
+  if (tamanho > 50) {
+    valorInput = '50';
+  } else if (tamanho < 5) {
+    valorInput = '5';
+  }
+  if (valorInput == '') {
+    alert('Board inválido!');
+  } else {
+    geraQuadro(valorInput);
+  }
+}
+
+function gerarCores() {
+  let cores = document.querySelector('#color-palette').children;
+  for (let index = 1; index < cores.length; index += 1){  
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    cores[index].style.backgroundColor = '#' + randomColor;
+  }
+}
+
 window.onload = function () {
+  geraQuadro('5');
+  gerarCores();
   corSelecionada = document.querySelector('#black');
   corSelecionada.classList.add('selected');
   const clearButton = document.querySelector('#clear-board');
+  const generateButton = document.querySelector('#generate-board');
+  const generateColorButton = document.querySelector('#generate-color');
   document.addEventListener('click', selecionaCor);
   document.addEventListener('click', pintaPixel);
   clearButton.addEventListener('click', limpaQuadro);
+  generateButton.addEventListener('click', checaValorTamanho);
+  generateColorButton.addEventListener('click', gerarCores);
 };
